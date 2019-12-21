8752f8a75ad71f13ac5126c5b4926d1f9d230974
skyying <dreamuk@gmail.com>
2019-12-21 16:11:30 +0800

    add solution 8

---
+++ 008/8.py
@@ -0,0 +1,24 @@
+
+
+def get_digits():
+    fp = open('./input.txt', "r")
+    content = ""
+    for line in iter(fp):
+        print(line)
+        content += line
+    fp.close()
+    return "".join(content.split("\n"))
+
+
+def get_product(str):
+    m = 1
+    for i in range(0, len(str)):
+        m *= int(str[i])
+    return m
+
+
+def max_product_in_a_series(n, data):
+    return max([get_product(data[i:i+n]) for i in range(len(data) - n + 1) ])
+
+
+print(max_product_in_a_series(13, get_digits()))
