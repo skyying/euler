26c865e177ba6a375176f877f1a8fe5b18f63e63
skyying <dreamuk@gmail.com>
2020-01-19 22:54:28 +0800

    add solution 23

---
+++ 023/23.py
@@ -0,0 +1,41 @@
+from collections import defaultdict
+import math
+import time
+from datetime import datetime
+
+# greate than 28123 is okay
+#n = 28123
+
+# this takes about 2 and half seconds to run
+
+
+def sum_of_divisors(n):
+    if n == 1:
+        return 1
+    root = math.ceil(math.sqrt(n))
+    divisors = []
+    for i in range(2, root):
+        if n % i == 0:
+            divisors += [i, n // i]
+    if root**2 == n:
+        divisors.append(root)
+    return sum(divisors) + 1
+
+
+def calc_non_abundant_sums(n):
+    made_by_two_abundants = {}
+    all_pos_sum_up_to_n = n * (n + 1) // 2
+    abundants = []
+
+    for i in range(1, n + 1):
+        if i < sum_of_divisors(i):
+            abundants.append(i)
+            for x in abundants:
+                if x + i <= n:
+                    made_by_two_abundants[x + i] = 1
+        if i in made_by_two_abundants:
+            all_pos_sum_up_to_n -= i
+    return all_pos_sum_up_to_n
+
+
+print(calc_non_abundant_sums(28123))
